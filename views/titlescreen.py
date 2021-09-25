750346c1352593202f76d9836e7afbff0c3759f1
astral8 <akulsingha8@gmail.com>
2021-09-24 20:38:34 -0400

    Added basic button to Title Screen

--- views/titlescreen.py
+++ views/titlescreen.py
@@ -10,11 +10,21 @@ from views.mainmenu import MainMenu
 class TitleScene(Scene):
     def __init__(self):
         super().__init__()
+        self.button = pygame.Rect(375, 450, 50, 50)
 
     def process_input(self, events, pressed_keys):
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    self.next_scene = MainMenu()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos  # gets mouse position
+
+                # checks if mouse position is over the button
+
+                if self.button.collidepoint(mouse_pos):
+                    # prints current location of mouse
                     self.next_scene = MainMenu()
 
     def update(self):
@@ -27,3 +37,4 @@ class TitleScene(Scene):
         rect.centerx = settings.SCREEN_WIDTH // 2
         rect.centery = settings.SCREEN_HEIGHT // 2
         settings.screen.blit(text, rect)
+        pygame.draw.rect(settings.screen, [255, 0, 0], self.button)
